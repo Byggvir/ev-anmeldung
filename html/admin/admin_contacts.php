cec854b3ed596a8bf796e5fbcba2443195eb0799
Thomas Arend <thomas@arend-rhb.de>
2020-11-30 00:25:18 +0100

    Contact form and menue buttons. DELETE selected contact

--- html/admin/admin_contacts.php
+++ html/admin/admin_contacts.php
@@ -48,25 +48,231 @@ include_once "../lib/contacts.php" ;
 filter_var_array($_POST, FILTER_SANITIZE_SPECIAL_CHARS);
 
 $DEBUG = false;
+$message = "";
 
 // Initialisierung
 
+if (array_key_exists('submit', $_POST)) {
+	$submit = $_POST['submit'];
+}
+else {
+	$submit = "";
+}
+
+$C = new contact($_POST);
+
+if (array_key_exists('contact', $_POST) and is_numeric($_POST['contact'])) {
+	$cid = $_POST['contact'];
+	$C->look_up($cid);
+} else {
+	$C->look_up(NULL);
+	$cid = $C->id;
+}
+
+if ( ! empty($submit) ) {
+    
+//  $message .= $submit;
+    
+    switch ($submit) {
+
+	case "ADD" :
+
+		$temp = $C->Firstname;
+		foreach ($Firstnames as $value) {
+			$C->id = NULL;
+
+			$C->Firstname = trim($value);
+			$message .= $value.": ";
+			$error = $C->check_contact();
+			if ($error == "" ) {
+				$message .= $C->add_contact($event) . '<br />';
+			} else {
+
+				$message .= "Fehlende oder unzulässige Daten in " . $error . '<br />';
+			}
+		}
+		$C->Firstname = $temp;
+
+		break;
+
+    case "DELETE" :
+
+		$delC = new contact ();
+		if ( ! empty($cid) and is_numeric($cid) ) {
+            $delC->look_up ($cid);
+            if ( ! is_null($delC->id)) {
+                $SQL = "delete from contacts where id = " . $delC->id . ";";
+                $message .= $SQL;
+                $delC->delete($SQL);
+            	$message .= "Kontakt " . $delC->Name . ', ' .$delC->Firstname . ' gelöscht<br />';
+			}
+        }
+		
+		break;
+
+	}
+}
 
 ?>
 
   
-<section class="adminbox">
+<section class="visitorbox box ">
 
 <h2>Kontaktdaten der Besucher</h2>
 
 <?php 
 
-$contacts = new contact($_POST);
-
-$contacts->list($contacts->id);
+$C->list($cid);
 
  
 ?>
+
+</section>
+<section class="box">
+
+<h2>Neuer Kontakt</h2>
+
+
+<table>
+
+<tr>
+<td>
+<label form="anmeldung">Vorname</label>
+</td>
+<td>
+<input type="text"
+    name="Firstname"
+    id="Firstname"
+	value="<?php echo $C->Firstname ; ?>"
+	maxlength="128" />
+</td>
+</tr>
+
+<tr>
+<td>
+<label form="anmeldung">Nachname</label>
+</td>
+<td>
+<input type="text"
+    name="Name"
+    id="Name"
+	value="<?php echo $C->Name ; ?>"
+	maxlength="64" />
+</td>
+</tr>
+
+<tr>
+<td>
+<label form="anmeldung">Straße</label>
+</td>
+<td>
+<input type="text"
+    name="Street"
+    id="Street"
+	value="<?php echo $C->Street ; ?>"
+	maxlength="64" />
+</td>
+</tr>
+
+<tr>
+<td>
+<label form="anmeldung">PLZ</label>
+</td>
+<td>
+<input type="text"
+    name="PostalCode"
+    id="PostalCode"
+	value="<?php echo $C->PostalCode; ?>"
+	maxlength="5" />
+</td>
+</tr>
+
+<tr>
+<td>
+<label form="anmeldung">Stadt</label>
+</td>
+<td>
+<input type="text"
+    name="City"
+    id="City"
+	value="<?php echo $C->City ; ?>"
+	maxlength="64" />
+</td>
+</tr>
+
+<tr>
+<td>
+<label form="anmeldung">Telefon</label>
+</td>
+<td>
+<input type="text"
+    name="Phone"
+    id="Phone"
+	value="<?php echo $C->Phone ; ?>"
+	maxlength="16" />
+</td>
+</tr>
+
+<tr>
+<td>
+<label form="anmeldung">E-Mail</label>
+</td>
+<td>
+<input type="text"
+    name="EMail"
+    id="EMail"
+	value="<?php echo $C->EMail ; ?>"
+	maxlength="64" />
+</td>
+</tr>
+
+</table>
+
+<div class="menue">
+
+    <div class="menuebutton">
+
+        <button id="submit"
+            name="submit"
+            type="submit"
+            value="ADD"
+            style="color: green; font-weight: bold;">Hinzuf&uuml;gen
+        </button>
+    
+    </div>
+
+    <div class="menuebutton">
+
+        <button id="submit"
+            name="submit"
+            type="submit"
+            value="DELETE"
+            style="color: green; font-weight: bold;">L&ouml;schen
+        </button>
+    
+    </div>
+
+        <div class="menuebutton">
+
+        <button id="submit"
+            name="submit"
+            type="submit"
+            value="UPDATE"
+            style="color: green; font-weight: bold;">&Auml;ndern
+        </button>
+    
+    </div>
+
+</div>
+<div class="message">
+<?php
+if ( $message != "" ) {
+
+	echo '<p>' . $message . '</p>';
+
+}
+?>
+</div>
 
 </section>
 
