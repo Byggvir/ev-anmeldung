b781bca23ddb1585a751d6e2d80a26539dbc67c8
Thomas Arend <thomas@arend-rhb.de>
2020-12-05 21:40:12 +0100

    Imports X-mas services

--- sql/HeiligAbend.sql
+++ sql/HeiligAbend.sql
@@ -1,22 +1,33 @@
 insert into eventgroups 
     values(NULL,"Stationsgottesdienst");
 insert into events values
-(NULL,2,"2020-12-24 14:00","2020-12-24 14:45","Stationsgottesdienst 1","Slot 1","Kindergarten",10,0,2),
-(NULL,2,"2020-12-24 14:10","2020-12-24 14:55","Stationsgottesdienst 2","Slot 2","Kindergarten",10,0,2),
-(NULL,2,"2020-12-24 14:20","2020-12-24 15:05","Stationsgottesdienst 3","Slot 3","Kindergarten",10,0,2),
-(NULL,2,"2020-12-24 14:30","2020-12-24 15:15","Stationsgottesdienst 4","Slot 4","Kindergarten",10,0,2),
-(NULL,2,"2020-12-24 14:40","2020-12-24 15:25","Stationsgottesdienst 5","Slot 5","Kindergarten",10,0,2),
-(NULL,2,"2020-12-24 14:50","2020-12-24 15:35","Stationsgottesdienst 6","Slot 6","Kindergarten",10,0,2),
-(NULL,2,"2020-12-24 15:00","2020-12-24 15:45","Stationsgottesdienst 7","Slot 7","Kindergarten",10,0,2),
-(NULL,2,"2020-12-24 15:10","2020-12-24 15:55","Stationsgottesdienst 8","Slot 8","Kindergarten",10,0,2),
-(NULL,2,"2020-12-24 15:20","2020-12-24 16:05","Stationsgottesdienst 9","Slot 9","Kindergarten",10,0,2),
-(NULL,2,"2020-12-24 15:30","2020-12-24 16:15","Stationsgottesdienst 10","Slot 10","Kindergarten",10,0,2),
-(NULL,2,"2020-12-24 15:40","2020-12-24 16:25","Stationsgottesdienst 11","Slot 11","Kindergarten",10,0,2),
-(NULL,2,"2020-12-24 15:50","2020-12-24 16:35","Stationsgottesdienst 12","Slot 12","Kindergarten",10,0,2),
-(NULL,2,"2020-12-24 16:00","2020-12-24 16:45","Stationsgottesdienst 13","Slot 13","Kindergarten",10,0,2),
-(NULL,2,"2020-12-24 16:10","2020-12-24 16:55","Stationsgottesdienst 14","Slot 14","Kindergarten",10,0,2),
-(NULL,2,"2020-12-24 16:20","2020-12-24 17:05","Stationsgottesdienst 15","Slot 15","Kindergarten",10,0,2),
-(NULL,2,"2020-12-24 16:30","2020-12-24 17:15","Stationsgottesdienst 16","Slot 16","Kindergarten",10,0,2),
-(NULL,2,"2020-12-24 16:40","2020-12-24 17:25","Stationsgottesdienst 17","Slot 17","Kindergarten",10,0,2),
-(NULL,2,"2020-12-24 16:50","2020-12-24 17:35","Stationsgottesdienst 18","Slot 18","Kindergarten",10,0,2),
-(NULL,2,"2020-12-24 17:00","2020-12-24 17:45","Stationsgottesdienst 19","Slot 19","Kindergarten",10,0,2);
+(NULL,2,"2020-12-24 14:00","2020-12-24 14:25","Stationsgottesdienst 1","Slot 1","Kindergarten",10,0,2),
+(NULL,2,"2020-12-24 14:10","2020-12-24 14:35","Stationsgottesdienst 2","Slot 2","Kindergarten",10,0,2),
+(NULL,2,"2020-12-24 14:20","2020-12-24 14:45","Stationsgottesdienst 3","Slot 3","Kindergarten",10,0,2),
+(NULL,2,"2020-12-24 14:30","2020-12-24 14:55","Stationsgottesdienst 4","Slot 4","Kindergarten",10,0,2),
+(NULL,2,"2020-12-24 14:40","2020-12-24 15:05","Stationsgottesdienst 5","Slot 5","Kindergarten",10,0,2),
+(NULL,2,"2020-12-24 14:50","2020-12-24 15:15","Stationsgottesdienst 6","Slot 6","Kindergarten",10,0,2),
+(NULL,2,"2020-12-24 15:00","2020-12-24 15:25","Stationsgottesdienst 7","Slot 7","Kindergarten",10,0,2),
+(NULL,2,"2020-12-24 15:10","2020-12-24 15:35","Stationsgottesdienst 8","Slot 8","Kindergarten",10,0,2),
+(NULL,2,"2020-12-24 15:20","2020-12-24 15:45","Stationsgottesdienst 9","Slot 9","Kindergarten",10,0,2),
+(NULL,2,"2020-12-24 15:30","2020-12-24 15:55","Stationsgottesdienst 10","Slot 10","Kindergarten",10,0,2),
+(NULL,2,"2020-12-24 15:40","2020-12-24 16:05","Stationsgottesdienst 11","Slot 11","Kindergarten",10,0,2),
+(NULL,2,"2020-12-24 15:50","2020-12-24 16:15","Stationsgottesdienst 12","Slot 12","Kindergarten",10,0,2),
+(NULL,2,"2020-12-24 16:00","2020-12-24 16:25","Stationsgottesdienst 13","Slot 13","Kindergarten",10,0,2),
+(NULL,2,"2020-12-24 16:10","2020-12-24 16:35","Stationsgottesdienst 14","Slot 14","Kindergarten",10,0,2),
+(NULL,2,"2020-12-24 16:20","2020-12-24 16:45","Stationsgottesdienst 15","Slot 15","Kindergarten",10,0,2),
+(NULL,2,"2020-12-24 16:30","2020-12-24 16:55","Stationsgottesdienst 16","Slot 16","Kindergarten",10,0,2),
+(NULL,2,"2020-12-24 16:40","2020-12-24 17:05","Stationsgottesdienst 17","Slot 17","Kindergarten",10,0,2),
+(NULL,2,"2020-12-24 16:50","2020-12-24 17:15","Stationsgottesdienst 18","Slot 18","Kindergarten",10,0,2);
+
+insert into eventgroups 
+    values(NULL,"Heiligabend");
+insert into events values
+(NULL,3,"2020-12-24 14:00","2020-12-24 14:25","Christvesper 1","Christvesper 1","Gnadenkirche",10,0,2),
+(NULL,3,"2020-12-24 14:10","2020-12-24 14:35","Christvesper 2","Christvesper 2","Gnadenkirche",10,0,2),
+(NULL,3,"2020-12-24 23:00","2020-12-24 23:45","Christnacht","Christnacht","Gnadenkirche",10,0,2);
+
+insert into events values
+(NULL,1,"2020-12-25 11:00","2020-12-25 11:45","1. Weihnachtstag","Weihnachten","Gnadenkirche",10,0,2),
+(NULL,1,"2020-12-26 10:00","2020-12-26 10:45","2. Weihnachtstag","Weihnachten","Gnadenkirche",10,0,2),
+(NULL,1,"2020-12-31 17:00","2020-12-31 18:00","Silvester","Altjahresabend","Gnadenkirche",10,0,2);
